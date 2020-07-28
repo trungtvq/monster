768c07bece820035fd16e4088b02d14c9d8af8c1
Truong Van Quoc Trung <truongvanquoctrung@mecury.com.vn>
2020-07-28 17:03:37 +0700

    erroe

--- src/services/firebase/initializeFirebase/index.js
+++ src/services/firebase/initializeFirebase/index.js
@@ -1,7 +1,14 @@
-import firebase from 'firebase';
-export const initializeFirebase = () => {
-  firebase.initializeApp({
-    // taken from your project settings --> cloud messaging
+import * as firebase from "firebase/app";
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+import '@firebase/messaging';
+
+const firebaseConfig = {
     apiKey: "AIzaSyAnr5HDbAz3-FueSWyJozHCRy3LNVcJtlc",
       authDomain: "refer-web.firebaseapp.com",
       databaseURL: "https://refer-web.firebaseio.com",
@@ -10,17 +17,17 @@ export const initializeFirebase = () => {
       messagingSenderId: "738408564499",
       appId: "1:738408564499:web:c10b4c0c62aaaa09befce6",
       measurementId: "G-ZNLK4KBRD2"
-  });
+};
+var app
+export const initializeFirebase = () => {
+  app = firebase.initializeApp(firebaseConfig);
 }
-navigator.serviceWorker
-    .register('./firebase-messaging-sw.js')
-    .then((registration) => {
-      firebase.messaging().useServiceWorker(registration);
-    });
-    
+
+
+
 export const askForPermissioToReceiveNotifications = async () => {
     try {
-      const messaging = firebase.messaging();
+      const messaging =  app.messaging();
       await messaging.requestPermission();
       const token = await messaging.getToken();
       console.log('token do usuário:', token);
